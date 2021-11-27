ec04fabd88da7f56495f7044efe5e6c26cc07b27
Andreas Blaette <andreas.blaette@uni-due.de>
2021-11-27 22:54:31 +0100

    initial commit

---
+++ R/coalagree-package.R
@@ -0,0 +1,9 @@
+#' coalagree-package
+#' 
+#' @author Andreas Blaette (andreas.blaette@@uni-due.de)
+#' @keywords package
+#' @docType package
+#' @aliases coalagree coalagree-package
+#' @rdname coalagree-package
+#' @name coalagree-package
+NULL
